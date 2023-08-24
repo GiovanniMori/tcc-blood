8139d1c4c6778feff0fd5b8cb6815f0cec2be08d
Giovanni Mori <giovanni_mori@hotmail.com>
2023-08-24 05:35:27 -0300

    feat: footer

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,9 @@
 import { ThemeModeToggle } from "@/components/theme-mode-toggle"
-import Image from "next/image"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ThemeModeToggle />
+      Login
     </main>
   )
 }
