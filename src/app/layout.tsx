bbf10e93dc42d48b0e0ca9fd259ce350b9a66172
Giovanni Mori <giovannimori@MacBook-Air-de-Giovanni.local>
2023-08-26 10:00:04 -0300

    feat: clerk ptBR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import Footer from "@/components/footer"
 import { ClerkProvider } from "@clerk/nextjs"
+import { ptBR } from "@clerk/localizations"
+
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -17,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider localization={ptBR}>
       <html lang="pt-BR" suppressHydrationWarning>
         <body className={inter.className}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
