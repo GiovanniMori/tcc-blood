7d835b17a94ab585a735b0650fef05bffb154a4f
Giovanni Mori <giovannimori@MacBook-Air-de-Giovanni.local>
2023-09-05 14:37:10 -0300

    fix: loader spinner props missing

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -2,17 +2,13 @@
 
 import React, { FunctionComponent } from "react"
 import { TailSpin } from "react-loader-spinner"
-import { BaseProps } from "react-loader-spinner/dist/type"
 
-type LoaderProps = BaseProps
-
-function Loader(props: LoaderProps) {
+function Loader() {
   return (
     <TailSpin
-      {...props}
-      height={props.height ? props.height : 16}
-      width={props.width ? props.width : 16}
-      color={props.color ? props.color : "#FFFFFF"}
+      height={16}
+      width={16}
+      color={"#FFFFFF"}
       ariaLabel="tail-spin-loading"
     />
   )
