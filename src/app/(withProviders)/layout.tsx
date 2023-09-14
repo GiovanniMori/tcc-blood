58f26dda2a0836cae218eadb2cd6d11957512e60
Giovanni Mori <giovannimori@MacBook-Air-de-Giovanni.local>
2023-09-14 11:33:35 -0300

    feat: prisma layout test

--- src/app/(withProviders)/layout.tsx
+++ src/app/(withProviders)/layout.tsx
@@ -9,14 +9,14 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const user = await currentUser();
-  const userDb = await prisma.user.findUnique({
-    where: {
-      id: user?.id,
-    },
-  });
-  if (userDb) {
-    !userDb && redirect("/register");
-  }
+  // const user = await currentUser();
+  // const userDb = await prisma.user.findUnique({
+  //   where: {
+  //     id: user?.id,
+  //   },
+  // });
+  // if (userDb) {
+  //   !userDb && redirect("/register");
+  // }
   return <>{children}</>;
 }
