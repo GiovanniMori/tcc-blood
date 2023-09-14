5cbd9f1859f2452ba9267aad4befc4112ac8f498
Giovanni Mori <giovannimori@MacBook-Air-de-Giovanni.local>
2023-09-14 10:10:57 -0300

    fix: remove react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,26 +27,20 @@ export default function RootLayout({
     <html lang="pt-BR" suppressHydrationWarning>
       <body className={inter.className}>
         <ClerkProvider localization={ptBR}>
-          <ReactQueryProvider>
-            <UserContextProvider>
-              <ThemeProvider
-                attribute="class"
-                defaultTheme="system"
-                enableSystem
-              >
-                <div className="relative flex min-h-screen flex-col">
-                  <Navbar />
-                  <div className="flex-1">
-                    <div className="container pt-6 overflow-x-hidden mb-8">
-                      oi
-                    </div>
+          <UserContextProvider>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+              <div className="relative flex min-h-screen flex-col">
+                <Navbar />
+                <div className="flex-1">
+                  <div className="container pt-6 overflow-x-hidden mb-8">
+                    oi
                   </div>
                 </div>
-                {/* <Footer /> */}
-              </ThemeProvider>
-              <Toaster />
-            </UserContextProvider>
-          </ReactQueryProvider>
+              </div>
+              {/* <Footer /> */}
+            </ThemeProvider>
+            <Toaster />
+          </UserContextProvider>
         </ClerkProvider>
       </body>
     </html>
