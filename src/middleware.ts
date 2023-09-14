efd25823996cc1d6b3b4ab08943a5d3c3e897dab
Giovanni Mori <giovannimori@MacBook-Air-de-Giovanni.local>
2023-09-14 09:43:18 -0300

    fix: middleware doar

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
-import { authMiddleware, redirectToSignIn } from "@clerk/nextjs"
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"],
-})
+  publicRoutes: ["/", "/doar"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-}
+};
