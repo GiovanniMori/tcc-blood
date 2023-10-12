8e459602f232f9ad8c662db4e6c00236327b8f2b
Giovanni Mori <giovannimori@MacBook-Air-de-Giovanni.local>
2023-10-12 18:53:45 -0300

    notfound page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>404</h2>
-      <p>Não conseguimos encontrar essa página</p>
-      <Button asChild>
-        <Link href="/"> Voltar ao início</Link>
-      </Button>
+    <div className="flex flex-col justify-center items-center gap-4">
+      <img src="/not-found.svg" alt="404" className=" max-w-xs " />
+      <div className="flex flex-col gap-2 text-center">
+        <p>Não conseguimos encontrar essa página</p>
+        <Button asChild>
+          <Link href="/"> Voltar ao início</Link>
+        </Button>
+      </div>
     </div>
   );
 }
